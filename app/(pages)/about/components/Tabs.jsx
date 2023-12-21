e7cd59abc1e62dcb056933cf2a5afd0d622921a6
Chisom <83100384+titanwarlord007@users.noreply.github.com>
2023-12-21 22:23:32 +0100

    ...

--- app/(pages)/about/components/Tabs.jsx
+++ app/(pages)/about/components/Tabs.jsx
@@ -1,12 +1,8 @@
 
-// export default function Tabs() {
-//     return (
-//         <div className="flex flex-col container py-8">
-//             <section id="about"></section>
-//             <section id="education"></section>
-//             <section id="certification"></section>
-//             <section id="experience"></section>
-//             <section id="skills"></section>
-//         </div>
-//     )
-// }
+export default function Tabs() {
+    return (
+        {
+           
+       }
+    )
+}
